17db1f822effc1f9de56928950b99ef2bbb88ac3
imsamue <97547135+imsamue@users.noreply.github.com>
2022-01-25 17:09:00 -0600

    Homework 1.15.22

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -4,15 +4,18 @@ let sun;
 
 function setup() {
   createCanvas(500, 500);
-  rectMode(CENTER);
+  background(0);
+  imageMode(CENTER);
 
+  fill(50, 100, 50);
+  rect(100, 0, 300, 500)
   sorin = loadImage("assets/sorin.jpeg");
   bed = loadImage("assets/catsbed.jpg");
   sun = loadImage("assets/catssun.jpg");
 }
 
 function draw() {
-  image(sorin, width/2, 100, 150, 150);
-  image(bed, width/2, 200, 100, 133);
-  image(sun, width/2, 300, 130, 100);
+  image(sorin, width/2, 100, 100, 100);
+  image(bed, width/2, 250, 100, 133);
+  image(sun, width/2, 400, 133, 100);
 }
