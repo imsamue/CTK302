fb363233b3e464eabaf90ed219f0ed865a2fb3b5
imsamue <97547135+imsamue@users.noreply.github.com>
2022-01-27 10:48:11 -0600

    mp1 Cat Face Only

--- Unit1/mp1/sketch.js
+++ Unit1/mp1/sketch.js
@@ -1,18 +1,169 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
   rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
-
-
   if (mouseIsPressed) {
-    background('#2D936C');
+    background("orange");
+    
+    //head
+    noStroke();
+    fill(255);
+    ellipse(width / 3, height / 3, 120, 120);
+
+    //nose
+    noStroke;
+    fill("pink");
+    triangle(
+      width / 3,
+      height / 3,
+      width / 3 - 10,
+      height / 3 + 10,
+      width / 3 + 10,
+      height / 3 + 10
+    );
+
+    //black fur patches
+    fill(0);
+    beginShape();
+    curveVertex(width / 3, 72);
+    curveVertex(width / 3 + 5, 72);
+    curveVertex(210, 100);
+    curveVertex(204, 120);
+    curveVertex(220, 130);
+    curveVertex(260, 120);
+    curveVertex(240, 90);
+    endShape();
+    arc(width / 3, height / 3, 120, 120, -84, -11, OPEN);
+
+    beginShape();
+    curveVertex(width / 3, 72);
+    curveVertex(width / 3 - 5, 72);
+    curveVertex(190, 100);
+    curveVertex(196, 120);
+    curveVertex(180, 130);
+    curveVertex(140, 120);
+    curveVertex(160, 90);
+    endShape();
+    arc(width / 3, height / 3, 120, 120, 191, 264, OPEN);
+
+    //ears
+    fill("black");
+    triangle(140, 60, 150, 110, 175, 80);
+    triangle(260, 60, 255, 110, 225, 80);
+    fill("pink");
+    triangle(148, 70, 155, 95, 170, 85);
+    triangle(252, 70, 250, 95, 230, 85);
+
+    //eyes
+    strokeWeight(0.25);
+    stroke(0);
+    fill(230, 230, 0);
+    ellipse(180, 115, 25, 15);
+    ellipse(220, 115, 25, 15);
+    fill(0);
+    ellipse(180, 115, 5, 10);
+    ellipse(220, 115, 5, 10);
+
+    //whiskers
+    strokeWeight(1);
+    stroke(180);
+    line(190, 142, 150, 150);
+    line(210, 142, 250, 150);
+    line(190, 139, 150, 139);
+    line(210, 139, 250, 139);
+    line(190, 136, 150, 128);
+    line(210, 136, 250, 128);
+
+    //mouth
+    stroke(0);
+    fill(200,0,100);
+    arc(width / 3, 145, 50, 60, 20, 160, CHORD);
+
   } else {
-    background("yellow");
+    background("navy");
+
+    //head
+    noStroke();
+    ellipse(width / 3, height / 3, 120, 120);
+
+    //nose
+    noStroke;
+    fill("pink");
+    triangle(
+      width / 3,
+      height / 3,
+      width / 3 - 10,
+      height / 3 + 10,
+      width / 3 + 10,
+      height / 3 + 10
+    );
+
+    //black fur patches
+    fill(0);
+    beginShape();
+    curveVertex(width / 3, 72);
+    curveVertex(width / 3 + 5, 72);
+    curveVertex(210, 100);
+    curveVertex(204, 120);
+    curveVertex(220, 130);
+    curveVertex(260, 120);
+    curveVertex(240, 90);
+    endShape();
+    arc(width / 3, height / 3, 120, 120, -84, -11, OPEN);
+
+    beginShape();
+    curveVertex(width / 3, 72);
+    curveVertex(width / 3 - 5, 72);
+    curveVertex(190, 100);
+    curveVertex(196, 120);
+    curveVertex(180, 130);
+    curveVertex(140, 120);
+    curveVertex(160, 90);
+    endShape();
+    arc(width / 3, height / 3, 120, 120, 191, 264, OPEN);
+
+    //ears
+    fill("black");
+    triangle(140, 60, 150, 110, 175, 80);
+    triangle(260, 60, 255, 110, 225, 80);
+    fill("pink");
+    triangle(148, 70, 155, 95, 170, 85);
+    triangle(252, 70, 250, 95, 230, 85);
+
+    //eyes
+    strokeWeight(0.25);
+    stroke(0);
+    fill(230, 230, 0);
+    ellipse(180, 115, 25, 15);
+    ellipse(220, 115, 25, 15);
+    fill(0);
+    ellipse(180, 115, 5, 10);
+    ellipse(220, 115, 5, 10);
+
+    //whiskers
+    strokeWeight(1);
+    stroke(180);
+    line(190, 142, 150, 150);
+    line(210, 142, 250, 150);
+    line(190, 139, 150, 139);
+    line(210, 139, 250, 139);
+    line(190, 136, 150, 128);
+    line(210, 136, 250, 128);
+
+    //mouth
+    stroke(0);
+    fill(0, 0);
+    arc(width / 3, 140, 40, 40, 30, 150);
+
+    //coordinates
+    fill("white");
+    text(mouseX + "," + mouseY, 20, 20);
   }
 
-
-  rect(width/2, height/2, 200, 100);
-
+  function mouseReleased() {
+    print(mouseX + "," + mouseY);
+  }
 }
