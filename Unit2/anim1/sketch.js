b11e4bc467e64fa1c7fb7bd1f1fc61ab49bee247
imsamue <97547135+imsamue@users.noreply.github.com>
2022-02-10 15:10:16 -0600

    Move text font

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -3,7 +3,7 @@ let y = 0;
 let font;
 
 function preLoad(){
-font = loadFont("assets/BADABB.TTF");  
+font = loadFont("assets/BADABB.TTF");
 }
 
 function setup() {
@@ -16,10 +16,10 @@ function draw() {
   background(0, 128, 128);
   fill(random (255), 0, random(255));
   textSize(random(70, 75));
-  textFont(font);
   push();
   translate(x, 200);
   rotate(y);
+  textFont(font);
   text("ZOOM", 0, 0);
   x = x + 5;
   if (x > width) {
