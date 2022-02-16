18f6d830c149b3df259cfc11e20f7846a3c72a34
imsamue <97547135+imsamue@users.noreply.github.com>
2022-02-15 21:40:17 -0600

    joke 1 rough

--- Unit2/joke1/sketch.js
+++ Unit2/joke1/sketch.js
@@ -1,7 +1,22 @@
+let font;
+state = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  font = loadFont("assets/school.ttf");
+  textAlign(CENTER, CENTER);
 }
 
 function draw() {
+switch(state){
+  case 0:
+  background (0, 128, 128);
+  textFont(font);
+  textSize(50);
+  text("How do you know when a\njoke is a Dad joke?", width / 2, height / 2);
+  break;
 
+  case 1:
+  break;
+}
 }
