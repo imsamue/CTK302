62e51f4bace866bb5809ca8d21ea50a565d38865
imsamue <97547135+imsamue@users.noreply.github.com>
2022-02-17 14:09:42 -0600

    multisound new font final

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -1,9 +1,11 @@
 let song1;
 let song2;
 let song3;
+let font;
 let state = 0;
 
 function preload() {
+  font = loadFont("assets/Panic.ttf");
   song1 = loadSound("assets/nine.mp4");
   song2 = loadSound("assets/gospel.mp4");
   song3 = loadSound("assets/hopes.mp4");
@@ -31,8 +33,9 @@ function draw() {
     case 1:
       background("purple");
       fill(255);
-      textSize(50);
-      text("9 in the Afternoon", width / 2, height / 4);
+      textSize(80);
+      textFont(font);
+      text("9 in the\nAfternoon", width / 2, height / 2);
       break;
 
     case 2:
@@ -43,7 +46,7 @@ function draw() {
     case 3:
       background("blue");
       fill(255);
-      text("This is Gospel", width / 2, height / 4);
+      text("This is Gospel", width / 2, height / 2);
       break;
 
     case 4:
@@ -54,7 +57,7 @@ function draw() {
     case 5:
       background("yellow");
       fill(0);
-      text("High Hopes", width / 2, height / 4);
+      text("High Hopes", width / 2, height / 2);
       break;
   }
 }
