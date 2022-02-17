4d8fd0f860c267f561a0c03cddb969c45529af40
imsamue <97547135+imsamue@users.noreply.github.com>
2022-02-17 17:37:26 -0600

    Added code bases

--- Unit2/button/sketch.js
+++ Unit2/button/sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  rectMode(CENTER);
 }
 
 function draw() {
+  background(255, 100, 100);
+  stroke(0, 0, 100);
+  strokeWeight(3);
+  fill(100, 100, 255);
+  rect(width / 2, height / 2, 150, 80, 20);
 
 }
