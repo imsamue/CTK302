4064c0468297a70494d1defc0d0ab4019fa49e8e
imsamue <97547135+imsamue@users.noreply.github.com>
2022-02-25 09:19:59 -0600

    touches test

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,35 +1,45 @@
-let numberOfTouches ;
+let numberOfTouches;
+let font1;
+let pic1, pic2, pic3;
 
 function setup() {
   createCanvas(400, 400);
+  font1 = loadFont("assets/song.ttf");
+  pic1 = loadImage("assets/lonely.jpg");
+  pic2 = loadImage("assets/two.jpg");
+  pic3 = loadImage("assets/three.jfif");
+  textAlign(CENTER);
+  imageMode(CENTER);
 }
 
 function draw() {
   clear();
+  background(0);
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+  fill(255);
+  textFont(font1);
+  textSize(24);
+  text(numberOfTouches + " touches", width / 2, 380);
 
-  switch(numberOfTouches) {
+  switch (numberOfTouches) {
     case 0:
-      text("no one is touching the screen", 5, 22) ;
-      break ;
+      textSize(36);
+      text("No one is touching the screen", width / 2, 40);
+      break;
 
-      case 1:
-       text("it's kind of lonely here", 5, 22) ;
-      // put a picture here
-      break ;
+    case 1:
+      text("It's kind of lonely here", width / 2, 40);
+      image(pic1, width / 2, height / 2, 400, 300);
+      break;
 
-      case 2:
-      text("two fingers are touching the screen", 5, 22) ;
-            // put a picture here
-      break ;
+    case 2:
+      text("Two fingers are touching the screen", width / 2, 40);
+      image(pic2, width / 2, height / 2, 400, 300);
+      break;
 
-      case 3:
-     text("three are touching the screen", 5, 22) ;
-            // put a picture here
-      break ;
-
-
+    case 3:
+      text("Three are touching the screen", width / 2, 40);
+     image(pic3, width / 2, height / 2, 400, 300);
+      break;
   }
-
 }
